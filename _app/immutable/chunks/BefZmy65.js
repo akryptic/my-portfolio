4d067ac4eade0c61c5aebf12643869fd2e41df77
Anil Kumar <akryptic00@gmail.com>
2025-04-27 07:28:12 +0530

    Oops

---
+++ _app/immutable/chunks/BefZmy65.js
@@ -0,0 +1,1 @@
+import{F as a}from"./DPhGc9wt.js";a();
--- src/lib/social.ts
+++
@@ -1,5 +0,0 @@
-export const EMAIL = 'akt01.social@gmail.com';
-export const GITHUB_USERNAME = 'akryptic';
-export const X_USERNAME = 'akryptic0';
-export const INSTAGRAM_USERNAME = 'akryptic0';
-export const LINKEDIN_USERNAME = '';
